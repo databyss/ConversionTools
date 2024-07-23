d850fa97ec5d7d083528fd35c939ee9c866ca38f
Andrew Henninger <databyss@gmail.com>
2024-07-23 12:32:29 -0400

    Initial commit
    The starting point for refactoring. Project explanation is in the README.md

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+const string filepath = "textfile.txt";
+
+const int bufferSize = 2;
+byte[] buffer = new byte[bufferSize];
+int bytesRead;
+string fromBytes;
+int intValue;
+byte byteVal;
+List<byte> allBytes = new List<byte>();
+int totalBytesRead = 0;
+
+using (var inputStream = new FileStream(filepath, FileMode.Open))
+{
+    do
+    {
+        bytesRead = inputStream.Read(buffer, 0, bufferSize);
+        totalBytesRead += bytesRead;
+        fromBytes = Encoding.ASCII.GetString(buffer);
+        intValue = int.Parse(fromBytes, System.Globalization.NumberStyles.HexNumber);
+        byteVal = Convert.ToByte(intValue);
+        Console.WriteLine($"{fromBytes} -> {intValue} -> {Convert.ToChar(intValue)}");
+        allBytes.Add(byteVal);
+    } while (bytesRead > 0);
+}
+
+Console.WriteLine($"Caught {allBytes.Count} total bytes");
+
+File.WriteAllBytes("C:\\Temp\\test.bmp", allBytes.ToArray());
---
+++ README.md
@@ -0,0 +1,9 @@
+# ConversionTools
+A utility for taking some bytes and turning them into other bytes.
+
+## Thoughts
+Initially I wrote this little utility to help a buddy out with a problem.
+
+After solving the problem at hand, I thought it might make for a fun project to experiement with refactoring and testing.
+
+Feel free to start from this same initial commit and find your own way towards making it over complicated and verbose!
